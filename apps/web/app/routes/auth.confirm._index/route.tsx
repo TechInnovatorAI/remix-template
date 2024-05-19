8b0bdadb472f69e305f8c3bde871a2dbb03b67a4
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2024-05-19 23:44:53 +0700

    Refactor authentication routing and introduce new AuthCallbackService (#5)
    * Refactor authentication routing and introduce new AuthCallbackService
    
    In this commit, redundant code in handling auth callbacks and token hash verifications was refactored to a new service 'AuthCallbackService'. This service simplifies these processes, reducing code repetition and enhancing readability. The auth-related routing paths have also been adjusted to utilize the new service.
    
    * Added @opentelemetry/exporter-jaeger dependency
    
    This commit adds a new dependency @opentelemetry/exporter-jaeger to our sentry package for providing the Jaeger tracing exporter, updates pnpm-lock file with the corresponding entry. Also, some dependencies across a few packages have been refreshed.
    
    * Remove extensive project details from README
    
    This commit removes extensive detail on project features, setup, deployment and other miscellany from the README file. The readme now simply states the project is under development and updates should be pulled daily.

--- apps/web/app/routes/auth.confirm._index/route.tsx
+++ apps/web/app/routes/auth.confirm._index/route.tsx
@@ -1,55 +1,20 @@
-import { type EmailOtpType } from '@supabase/supabase-js';
-
 import { redirect } from '@remix-run/react';
 import type { LoaderFunctionArgs } from '@remix-run/server-runtime';
 
+import { createAuthCallbackService } from '@kit/supabase/auth';
 import { getSupabaseServerClient } from '@kit/supabase/server-client';
 
 import pathsConfig from '~/config/paths.config';
 
-const defaultNextUrl = pathsConfig.app.home;
-
 export async function loader({ request }: LoaderFunctionArgs) {
-  const { searchParams } = new URL(request.url);
+  const service = createAuthCallbackService(getSupabaseServerClient(request));
 
-  const token_hash = searchParams.get('token_hash');
-  const type = searchParams.get('type') as EmailOtpType | null;
-  const next = searchParams.get('next') ?? defaultNextUrl;
-  const callbackParam = searchParams.get('callback');
-  const callbackUrl = callbackParam ? new URL(callbackParam) : null;
-  const inviteToken = callbackUrl?.searchParams.get('invite_token');
+  const url = await service.verifyTokenHash(request, {
+    joinTeamPath: pathsConfig.app.joinTeam,
+    redirectPath: pathsConfig.app.home,
+  });
 
-  let pathname = next;
-  const nextSearchParams = new URLSearchParams();
-
-  // if we have an invite token, we append it to the redirect url
-  if (inviteToken) {
-    // if we have an invite token, we redirect to the join team page
-    // instead of the default next url. This is because the user is trying
-    // to join a team and we want to make sure they are redirected to the
-    // correct page.
-    pathname = pathsConfig.app.joinTeam;
-    nextSearchParams.set('invite_token', inviteToken);
-  }
-
-  if (token_hash && type) {
-    const supabase = getSupabaseServerClient(request);
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-
-    // if no error - proceed to the next page
-    if (!error) {
-      return redirect(pathname + '?' + nextSearchParams.toString(), {
-        headers: request.headers,
-      });
-    }
-  }
-
-  // return the user to an error page with some instructions
-  pathname = '/auth/callback/error';
-
-  return redirect(pathname + '?' + nextSearchParams.toString());
+  return redirect(url.href, {
+    headers: request.headers,
+  });
 }
