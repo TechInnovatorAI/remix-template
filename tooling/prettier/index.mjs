3cf5ecf34c44ea16ebdc93144ac61c667db51df2
gbuomprisco <giancarlopsk@gmail.com>
2024-06-12 00:13:17 +0800

    Update package versions and modify prettier config
    This revision updates various package versions mainly related to '@tanstack/react-query', '@typescript-eslint/eslint-plugin', etc. It also modifies the types configuration in the prettier index file by removing 'TailwindConfig'.

--- tooling/prettier/index.mjs
+++ tooling/prettier/index.mjs
@@ -1,8 +1,7 @@
 /** @typedef  {import("prettier").Config} PrettierConfig */
-/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 /** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | SortImportsConfig } */
 const config = {
   tabWidth: 2,
   useTabs: false,
