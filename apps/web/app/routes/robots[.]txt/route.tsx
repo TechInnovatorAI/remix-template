87eedf7d5510a903e75d21f3180c344bdfde897d
gbuomprisco <giancarlopsk@gmail.com>
2024-06-20 19:42:28 +0800

    Add robots.txt and sitemap.xml routes
    Introduced robots.txt and sitemap.xml routes in the web application. The robots.txt file is configured to allow access to all routes. The sitemap.xml file is generated dynamically using a list of predefined paths and content items fetched from the CMS. Both files are generated with UTF-8 encoding.

---
+++ apps/web/app/routes/robots[.]txt/route.tsx
@@ -0,0 +1,26 @@
+import appConfig from '~/config/app.config';
+
+const robots = {
+  rules: {
+    userAgent: '*',
+    allow: '/',
+  },
+  sitemap: `${appConfig.url}/sitemap.xml`,
+};
+
+export const loader = async () => {
+  const items = [
+    `User-agent: ${robots.rules.userAgent}`,
+    `Allow: ${robots.rules.allow}`,
+    `Sitemap: ${robots.sitemap}`
+  ];
+
+  const content = items.reduce((acc, curr) => acc + curr + '\n', '');
+
+  return new Response(content, {
+    status: 200,
+    headers: {
+      encoding: 'UTF-8',
+    },
+  });
+};
---
+++ apps/web/app/routes/sitemap[.]xml/route.tsx
@@ -0,0 +1,74 @@
+import { createCmsClient } from '@kit/cms';
+
+import appConfig from '~/config/app.config';
+
+export const loader = async () => {
+  const paths = getPaths();
+  const contentItems = await getContentItems();
+
+  const allUrls = [...paths, ...contentItems].map((path) => {
+    return {
+      loc: new URL(path, appConfig.url).href,
+      lastmod: new Date().toISOString(),
+    };
+  });
+
+  const urls = allUrls.map(
+    (url) =>
+      `<url>
+      <loc>${url.loc}</loc>
+      <lastmod>${url.lastmod}</lastmod>
+      <priority>1.0</priority>
+    </url>`,
+  );
+
+  const content = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+    </urlset>
+    `.trim();
+
+  return new Response(content, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+      'xml-version': '1.0',
+      encoding: 'UTF-8',
+    },
+  });
+};
+
+function getPaths() {
+  return [
+    '/',
+    '/faq',
+    '/blog',
+    '/docs',
+    '/pricing',
+    '/contact',
+    '/cookie-policy',
+    '/terms-of-service',
+    '/privacy-policy',
+    // add more paths here
+  ];
+}
+
+async function getContentItems() {
+  const client = await createCmsClient();
+
+  const posts = client
+    .getContentItems({
+      collection: 'posts',
+    })
+    .then((response) => response.items)
+    .then((posts) => posts.map((post) => `/blog/${post.slug}`));
+
+  const docs = client
+    .getContentItems({
+      collection: 'documentation',
+    })
+    .then((response) => response.items)
+    .then((docs) => docs.map((doc) => `/docs/${doc.slug}`));
+
+  return Promise.all([posts, docs]).then((items) => items.flat());
+}
