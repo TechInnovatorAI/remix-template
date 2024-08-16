270b6c00ff95135703f077c9ff2c3f224eaeb309
gbuomprisco <giancarlopsk@gmail.com>
2024-08-16 12:53:56 +0200

    Add onClose callback to LemonSqueezyEmbeddedCheckout
    Introduced an optional onClose prop to handle checkout closing events. The onClose function is triggered when the 'PaymentMethodUpdate.Closed' event occurs, ensuring proper cleanup and behavior for embedded checkouts. Adjusted the useLoadScript hook to support this new functionality and clean up the script element on component unmount.

--- packages/billing/lemon-squeezy/src/components/lemon-squeezy-embedded-checkout.tsx
+++ packages/billing/lemon-squeezy/src/components/lemon-squeezy-embedded-checkout.tsx
@@ -9,7 +9,6 @@ interface LemonSqueezyWindow extends Window {
       eventHandler: (event: { event: string }) => void;
     }) => void;
     Refresh: () => void;
-
     Url: {
       Open: (url: string) => void;
       Close: () => void;
@@ -17,13 +16,19 @@ interface LemonSqueezyWindow extends Window {
   };
 }
 
-export function LemonSqueezyEmbeddedCheckout(props: { checkoutToken: string }) {
-  useLoadScript(props.checkoutToken);
+export function LemonSqueezyEmbeddedCheckout(props: {
+  checkoutToken: string;
+  onClose?: () => void;
+}) {
+  useLoadScript(props.checkoutToken, props.onClose);
 
   return null;
 }
 
-function useLoadScript(checkoutToken: string) {
+function useLoadScript(
+  checkoutToken: string,
+  onClose: (() => void) | undefined,
+) {
   useEffect(() => {
     const script = document.createElement('script');
 
@@ -34,8 +39,22 @@ function useLoadScript(checkoutToken: string) {
 
       win.createLemonSqueezy();
       win.LemonSqueezy.Url.Open(checkoutToken);
+
+      if (onClose) {
+        win.LemonSqueezy.Setup({
+          eventHandler: (event) => {
+            if (event.event === 'PaymentMethodUpdate.Closed') {
+              onClose();
+            }
+          },
+        });
+      }
     };
 
     document.body.appendChild(script);
-  }, [checkoutToken]);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [checkoutToken, onClose]);
 }
